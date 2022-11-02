e32f450146ad80e7af0711b13ba7421045956b26
EikoM-Off <zar.jar@mail.ru>
2022-11-02 14:47:14 +0600

    fix

---
+++ src/layout/CardCustom.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-component flex flex-column p-3">
+    <div class="">
+      <slot name="header" />
+    </div>
+    <div class="text-700 text-3xl text-black-alpha-80 font-bold pt-2 pb-2">
+      <slot name="title" />
+    </div>
+    <div class="flex-grow-1">
+      <slot name="content" />
+    </div>
+    <div class="pt-2 pb-2">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardCustom'
+}
+</script>
+
+<style scoped></style>
